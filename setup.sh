3b5d69c037690329ba7f9eb3dd67bf9c3f740cd7
lc0des <lcodes@lightning.codes>
2021-07-19 12:24:43 +0200

    .

--- setup.sh
+++ setup.sh
@@ -202,7 +202,7 @@ function build_tor {
 	# start container
 	docker run  -d --restart=always --net=$dc-net --ip=$tor_ip -v $dc-vol-tor:/app/data/ --name $dc-tor $dc-tor
 	echo "Waiting for $dc-tor to come up"
-	sleep 10
+	sleep 30
 
 	# create TOR hashed password
 	gen_pass=`cat /dev/urandom | xxd -l 23 -p -u -c 23|sed -r 's/\s+//g'`
